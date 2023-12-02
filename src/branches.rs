ff1a05ceb8b20213163f0af0b860b2a4691a1041
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-12-02 15:13:02 +0530

    feat(KAN-5): added checkout functionality

--- src/branches.rs
+++ src/branches.rs
@@ -30,17 +30,8 @@ pub fn delete_branch(){
     let output = Command::new("git").arg("branch").arg("-D").arg(&selected_branch).output().expect("Failed to delete branch");
     helpers::status_printer(&output);
 }
-pub fn branch_manager(){
-    let branch_prompts = ["Checkout", "Create Branch", "Delete Branch", "Merge Branch", "Push Branch", "Pull Branch","View Branches","Switch Branch"];
-    let branch_prompt_selection = FuzzySelect::with_theme(&ColorfulTheme::default()).with_prompt("Select Branch Operation").items(&branch_prompts).interact().unwrap();
-    if branch_prompt_selection == 1{
-        create_branch();
-    }
-    if branch_prompt_selection == 2{
-        delete_branch();
-    }
-    if branch_prompt_selection == 6{
-        let output  = get_branches();
+pub fn view_branches(){
+    let output  = get_branches();
         if output.is_empty(){
             let message = "Error Fetching Branches";
             println!("{}",Style::new().for_stderr().red().apply_to(&message));
@@ -48,5 +39,28 @@ pub fn branch_manager(){
         for branch in output{
             println!("{}",Style::new().for_stdout().green().apply_to(&branch));
         }
+}
+pub fn checkout_branch(){
+    let branch_list = get_branches();
+    let branch_option = FuzzySelect::with_theme(&ColorfulTheme::default()).with_prompt("Select Branch to Checkout").items(&branch_list).interact().unwrap();
+    let selected_branch = &branch_list[branch_option];
+    let output = Command::new("git").arg("checkout").arg(&selected_branch).output().expect("Failed to checkout branch");
+    helpers::status_printer(&output);
+}
+pub fn branch_manager(){
+    let branch_prompts = ["Checkout", "Create Branch", "Delete Branch", "Merge Branch", "Pull Branch","View Branches"];
+    let branch_prompt_selection = FuzzySelect::with_theme(&ColorfulTheme::default()).with_prompt("Select Branch Operation").items(&branch_prompts).interact().unwrap();
+
+    if branch_prompt_selection == 0{
+        checkout_branch();
+    }
+    if branch_prompt_selection == 1{
+        create_branch();
+    }
+    if branch_prompt_selection == 2{
+        delete_branch();
+    }
+    if branch_prompt_selection == 5{
+        view_branches();
     }
 }
